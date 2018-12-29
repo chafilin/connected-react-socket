b4251aaa90565c21a3a49916f99465f3d67cb874
chafilin <chafiliny@gmail.com>
2018-12-29 23:01:26 +0300

    refactor: change context usage

--- src/withSocket.js
+++ src/withSocket.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import Context from './Context'
+import React from "react"
+import Context from "./Context"
 
 /**
  * Pass socket data and methods to component
  * @param {Object} WrappedComponent in which socket method should be injected
  * @returns {Object} injected component
  */
-function withSocket (WrappedComponent) {
+function withSocket(WrappedComponent) {
   /**
    * New component with socket methods
    */
-  class WithSocket extends React.Component {
-    render(){
-      const {payload,emit} = this.context
-      return <WrappedComponent socketEvent = {payload} emit={emit} {...this.props}/>
+  return class WithSocket extends React.Component {
+    render() {
+      return (
+        <Context.Consumer>
+          {({ payload, emit }) => (
+            <WrappedComponent
+              socketEvent={payload}
+              emit={emit}
+              {...this.props}
+            />
+          )}
+        </Context.Consumer>
+      )
     }
   }
-
-  WithSocket.contextType = Context
-
-  return WithSocket
 }
 
 export default withSocket
