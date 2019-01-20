67bd63d5a3b7a93b264219ac9405203b4c44e40f
chafilin <chafiliny@gmail.com>
2019-01-20 13:53:09 +0300

    [test] add socketProvider tests

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   collectCoverageFrom: ["src/*.{js,jsx}", "!src/index.js", "!src/Context.js"],
   coverageThreshold: {
     global: {
-      statements: 60,
-      branches: 70,
-      functions: 70,
-      lines: 50,
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85,
     },
   },
   verbose: true,
--- tests/SocketProvider.test.js
+++ tests/SocketProvider.test.js
@@ -1,5 +1,40 @@
-describe("mocked test", () => {
-  it("shouldn't have test", () => {
-    expect(true).toBe(true)
+import Enzyme from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import React from "react"
+import SocketProvider from "../src/SocketProvider"
+
+Enzyme.configure({ adapter: new Adapter() })
+const { mount } = Enzyme
+
+describe("Socket provider", () => {
+  let props, wrapper
+  beforeEach(() => {
+    props = {
+      socket: {
+        close: jest.fn(),
+      },
+    }
+    wrapper = mount(
+      <SocketProvider {...props}>
+        <div />
+      </SocketProvider>
+    )
+  })
+  it("should add listener on events", () => {
+    expect(props.socket.onevent).toBeDefined()
+  })
+
+  it("should add set payload in state", () => {
+    const message = {
+      data: ["hello", "world"],
+    }
+    const payload = { type: "hello", data: ["world"] }
+    props.socket.onevent(message)
+    expect(wrapper.state("payload")).toEqual(payload)
+  })
+
+  it("should close on unmount", () => {
+    wrapper.unmount()
+    expect(props.socket.close).toBeCalled()
   })
 })
