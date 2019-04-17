9081971f357bb14500d6129ee6bd63eee36e3fa3
Chafilin <chafiliny@gmail.com>
2019-04-17 17:45:48 +0300

    [demo] return demo

---
+++ demo/app/index.js
@@ -0,0 +1,71 @@
+/***  examples/src/index.js ***/
+/* eslint-disable */
+import React from "react"
+import { render } from "react-dom"
+import { Provider as ReduxProvider, connect } from "react-redux"
+
+import {
+  ConnectedSocket,
+  withSocket,
+  SocketProvider,
+  emitMessage,
+  injectSocket,
+} from "../../src"
+import store from "./store"
+
+import { socket1, socket2 } from "./socket"
+
+const Display = props => {
+  const { socketEvent, name, emit, socket } = props
+  console.log(socket)
+  return (
+    <div>
+      <h2>Event from {name}:</h2>
+      {socketEvent && socketEvent.time  && (
+        <div>Server timer is {socketEvent.time} </div>
+      )}
+      {socketEvent && socketEvent.id && (
+        <div>
+          Your id is <b>{socketEvent.id}</b>
+        </div>
+      )}
+      <button onClick={() => emit("reset")}>Reset</button>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    socketEvent: state.socket,
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    emit: (type, ...message) => dispatch(emitMessage(type, message)),
+  }
+}
+
+const ConnectedDisplay = injectSocket(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Display)
+)
+
+const DisplayEvent = injectSocket(withSocket(Display))
+
+const App = () => (
+  <div>
+    <SocketProvider socket={socket1}>
+      <h1>Demo socket provider</h1>
+      <DisplayEvent name="WithSocket" />
+    </SocketProvider>
+    <ReduxProvider store={store}>
+      <ConnectedSocket socket={socket2}>
+        <h1>Demo connected socket</h1>
+        <ConnectedDisplay name="connect" />
+      </ConnectedSocket>
+    </ReduxProvider>
+  </div>
+)
+render(<App />, document.getElementById("root"))
---
+++ demo/server/index.js
@@ -0,0 +1,30 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+const connections = {}
+
+io.on('connection', function(socket){
+	console.log('a user connected')
+	const {id} = socket
+  connections[id] = { socket }
+  connections[id].startTime = new Date()
+	connections[id].interval = setInterval(()=>{
+    const timer = Math.floor((new Date()-connections[id].startTime)/1000)
+		socket.emit('time',timer )
+	}, 1000)
+  socket.emit('id', id)
+  socket.on('reset', function(){
+    connections[id].startTime = new Date()
+  })
+  socket.on('disconnect', function(){
+		const { interval } = connections[id]
+		clearInterval(interval)
+		delete connections[id]
+    console.log(`user ${id} disconnected`)
+  })
+})
+
+http.listen(8081, function(){
+  console.log('listening on *:8081')
+})
